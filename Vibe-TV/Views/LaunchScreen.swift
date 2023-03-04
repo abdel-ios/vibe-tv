2fbc1abc0e435d4dfdace6b84084cd2e1ab15a94
abdel-ios <abdeldeveloper01@gmail.com>
2023-03-04 21:34:43 +0100

    Creating all the assets and helpers and setting the Looping video for WelcomeScreen

--- Vibe-TV/Views/LaunchScreen.swift
+++ Vibe-TV/Views/LaunchScreen.swift
@@ -11,20 +11,26 @@ struct LaunchScreen: View {
     @State private var isActive: Bool = false
     
     var body: some View {
-        ZStack {
-            Color.black.ignoresSafeArea()
+        if isActive{
+            WelcomeScreen()
+        }else{
             
-            LottieView(filename: "vibe-tv")
-                .frame(width: 200, height: 200)
-        }
-        .preferredColorScheme(.dark)
-        .onAppear{
-            DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
-                withAnimation {
-                    self.isActive =  true
+            ZStack {
+                Color.black.ignoresSafeArea()
+                
+                LottieView(filename: "vibe-tv")
+                    .frame(width: 200, height: 200)
+            }
+            .preferredColorScheme(.dark)
+            .onAppear{
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
+                    withAnimation {
+                        self.isActive =  true
+                    }
                 }
             }
         }
+        
         
     }
 }
